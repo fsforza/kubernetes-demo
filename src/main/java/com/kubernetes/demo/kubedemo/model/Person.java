c8ea11aec079d2ed847ddbb031d1d9851e26bba1
fsforza <fabio.sforza@outlook.it>
2019-01-16 14:17:59 +0100

    fix mysql support

--- src/main/java/com/kubernetes/demo/kubedemo/model/Person.java
+++ src/main/java/com/kubernetes/demo/kubedemo/model/Person.java
@@ -2,6 +2,7 @@ package com.kubernetes.demo.kubedemo.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 /**
@@ -11,7 +12,7 @@ import javax.persistence.Id;
 public class Person {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String name;
